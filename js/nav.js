51bdb778e2159da0ad6631aa805c2a0ffafb6e12
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-07-02 17:30:32 -0300

    Implemento animación en el menú desplegable

--- js/nav.js
+++ js/nav.js
@@ -4,17 +4,19 @@ const menuDesplegable = document.querySelector(".menu-desplegable-oculto")
 imgHamburguesa.addEventListener("click", ()=>{
     if (imgHamburguesa.src.includes("hamburguesa")) {
         imgHamburguesa.src = "iconos/menu-cruz.svg";
-        menuDesplegable.style.display = "flex"; 
+        // menuDesplegable.style.display = "flex"; 
+        menuDesplegable.classList.add("mostrar-menu")
     }else{
         imgHamburguesa.src = "iconos/menu-hamburguesa.svg";
-        menuDesplegable.style.display = "none"; 
+        // menuDesplegable.style.display = "none"; 
+        menuDesplegable.classList.remove("mostrar-menu")
     }
 })
 
 // este resize lo pongo por si esta abierto el desplegable y se agranda la pantalla, que se cierre
 window.addEventListener("resize", ()=>{
     if(window.innerWidth > 835){
-        menuDesplegable.style = "display: none;";
+        menuDesplegable.classList.remove("mostrar-menu");
         imgHamburguesa.src = "iconos/menu-hamburguesa.svg";
     }
 })
@@ -24,6 +26,12 @@ window.addEventListener("resize", ()=>{
 document.addEventListener("click", (e)=>{
     if (!menuDesplegable.contains(e.target) && !imgHamburguesa.contains(e.target)) {
         imgHamburguesa.src = "iconos/menu-hamburguesa.svg";
-        menuDesplegable.style = "display: none";
+        menuDesplegable.classList.remove("mostrar-menu");
     }
 });
+
+// le puse este q cuando la pestaña detecte un scroll, cierro el menú je
+window.addEventListener("scroll", ()=>{
+    imgHamburguesa.src = "iconos/menu-hamburguesa.svg";
+    menuDesplegable.classList.remove("mostrar-menu");
+})
