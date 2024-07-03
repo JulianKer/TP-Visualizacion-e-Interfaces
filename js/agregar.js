f66ad3ec6678a4ea035a83ec0c595b097d689cd7
FeliWeigel <fweigeldev@gmail.com>
2024-07-03 16:22:24 -0300

    Implementacion agregar turno clasico

--- js/agregar.js
+++ js/agregar.js
@@ -1,6 +1,19 @@
 const agregarVehiculoBtn = document.getElementById('agregar-vehiculo-btn')
 const popUpAgregarVehiculo = document.getElementById("popup-agregar-vehiculo");
 
-agregarVehiculoBtn.addEventListener("click", ()=>{
-    popUpAgregarVehiculo.style.display = "flex";
-})
+const agregarTurnoBtn = document.getElementById('agregar-turno-btn')
+const popUpAgregarTurno = document.getElementById("popup-agregar-turno");
+
+if(agregarTurnoBtn){
+    agregarTurnoBtn.addEventListener("click", ()=>{
+        popUpAgregarTurno.style.display = "flex";
+    })
+}
+
+if(agregarVehiculoBtn){
+    agregarVehiculoBtn.addEventListener("click", ()=>{
+        popUpAgregarVehiculo.style.display = "flex";
+    })
+}
+
+
