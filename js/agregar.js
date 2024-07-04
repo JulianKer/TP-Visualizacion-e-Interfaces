f8c02233f7fb7614e38e3edb2008317614f3b891
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2024-07-04 11:58:48 -0300

    Implementé el popup en 'constanciaTurno' y 'constanciaTurnoRapido' para cuando apretas el btn cancelar con sus respectivas redirecciones

--- js/agregar.js
+++ js/agregar.js
@@ -4,6 +4,12 @@ const popUpAgregarVehiculo = document.getElementById("popup-agregar-vehiculo");
 const agregarTurnoBtn = document.getElementById('agregar-turno-btn')
 const popUpAgregarTurno = document.getElementById("popup-agregar-turno");
 
+const cancelarTunoBtn = document.getElementById("cancelar-tunro-btn");
+const popUpCancelarTurno = document.getElementById("popup-cancelar-turno")
+
+console.log(cancelarTunoBtn)
+console.log(popUpCancelarTurno)
+
 if(agregarTurnoBtn){
     agregarTurnoBtn.addEventListener("click", ()=>{
         popUpAgregarTurno.style.display = "flex";
@@ -13,6 +19,12 @@ if(agregarTurnoBtn){
 if(agregarVehiculoBtn){
     agregarVehiculoBtn.addEventListener("click", ()=>{
         popUpAgregarVehiculo.style.display = "flex";
+    })
+}
+
+if(cancelarTunoBtn){
+    cancelarTunoBtn.addEventListener("click", ()=>{
+        popUpCancelarTurno.style.display = "flex";
     })
 }
 
